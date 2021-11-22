81cf8ca3d1d510eaa5414d7801ea0ef2dbbbeda3
Aiebel.ooo <geamdjum@gmail.com>
2021-11-22 15:02:19 +0000

    change

--- practice_find.js
+++ practice_find.js
@@ -1,11 +1,11 @@
-function theOneFine(){
+function theOneFine(dbed,collect){
     var MongoClient = require('mongodb').MongoClient;
   var url = "mongodb://localhost:27017/";
 
   MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("guigui");
-    dbo.collection("people").find({}).toArray(function(err, result) {
+    var dbo = db.db(dbed);
+    dbo.collection(collect).find({}).toArray(function(err, result) {
       if (err) throw err;
 
       result.forEach((element,i) => {
@@ -17,7 +17,8 @@ function theOneFine(){
   });
   }
 
-  theOneFine()
+  //theOneFine("資料庫","資料夾")
+// theOneFine("guigui","people")
   // 對資料庫 guigui 的 people資料夾 查看所有資料!
   // 5行 {useUnifiedTopology: true} 要對應方案修正，不然怪怪的
   //  有些是{ useNewUrlParser: true }
@@ -39,8 +40,29 @@ function theOneFine(){
   }
 
   // insertData("資料庫","資料夾","名子","生日")
-  insertData("guigui","people","小鴨鴨","1995/02/15")
+//insertData("guigui","people","小鴨鴨","1995/02/15")
   // 對資料庫 guigui 的 people資料夾 增加一筆'小鴨鴨'!
   // 5行 {useUnifiedTopology: true} 要對應方案修正，不然怪怪的
   // 有些是{ useNewUrlParser: true }
 
+
+  function fineLimit(dbed,collect,Limit){
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://localhost:27017/";
+
+  MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db(dbed);
+    dbo.collection(collect).find().limit(Limit).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
+}
+
+  //fineLimit("資料庫","資料夾",查詢幾筆資料只能數字)
+// fineLimit(dbed,collect,3);
+  //對資料庫 guigui 的 people資料夾 增加一筆'小鴨鴨'!
+  //5行 {useUnifiedTopology: true} 要對應方案修正，不然怪怪的
+  //有些是{ useNewUrlParser: true }
